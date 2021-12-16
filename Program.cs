5af50bd815650aa720f9986b87dda8547cc48116
Luis Ángel Santos <luis1337@outlook.es>
2021-12-16 22:41:43 +0100

    Comentadas clases principales

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿/**
+ * COM2Mem
+ * Programa de lanzamiento de instrucciones por puerto Serie
+ * 
+ * @author Luis Santos  <luis1337@outlook.es>
+ * @version 0.0.3a
+ */
+
+using System;
 using System.Collections.Generic;
 using System.IO.Ports;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace COM2Mem
 {
@@ -14,27 +19,38 @@ namespace COM2Mem
         {
             COM2Mem c2m = new COM2Mem();
 
+            // Hasta que termine la ejecución
             while( !c2m.isHalted )
             {
-                c2m.loop();
+                c2m.loop(); // Lanzamos el bucle
             }
 
         }
     }
 
+    /**
+     * Clase COM2Mem, conforma la interfaz principal
+     */
     class COM2Mem
     {
-        private List<MemPort> ports;
-        private int timeSleep;
+        private List<MemPort> ports;    // Lista de puertos
+        private int timeSleep;          // Tiempo de actualización
 
+        // Indica si termina la ejecución del programa
         public bool isHalted { get; set; } = false;
 
+        /**
+         * Constructor principal de la clase
+         */
         public COM2Mem()
         {
             ports = this.getPortList();
             timeSleep = 200;
         }
 
+        /**
+         * Bucle principal, se debe de lanzar contínuamente
+         */
         public void loop()
         {
             displayHeader();
@@ -66,6 +82,9 @@ namespace COM2Mem
             return spList;
         }
 
+        /**
+         * Muestra la cabecera de la interfaz
+         */
         private void displayHeader()
         {
             Console.WriteLine(
@@ -75,21 +94,31 @@ namespace COM2Mem
 
     }
 
+    /**
+     * Clase de abstracción de los puertos
+     */
     class MemPort
     {
-        public SerialPort port;
-        private int toOpen = 0;
-        private int toClose = 0;
-        private int toSend = 0;
-        private string toSendCmd;
+        public SerialPort port;     // Puerto real
+        private int toOpen = 0;     // Comando de apertura
+        private int toClose = 0;    // Comando de cierre
+        private int toSend = 0;     // Comando de envío
+        private string toSendCmd;   // String del comando
 
-
+        /**
+         * Inicializar cada puerto con su nombre
+         * 
+         * @param String portName Nombre del puerto
+         */
         public MemPort( String portName )
         {
             port = new SerialPort( portName );
             toSendCmd = $"CMD#{portName}\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
         }
 
+        /**
+         * Muestra las opciones para cada puerto
+         */
         public void display()
         {
             Console.WriteLine(
